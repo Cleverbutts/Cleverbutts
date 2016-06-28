73d00b0f8eac0faaca0465c5120adca4366d8c12
SplitPixl <splitpixl@gmail.com>
2016-06-28 09:31:09 -0700

    Accounts!
    Use as many (or as few) Bot accounts as you want.

--- cleverbutts.js
+++ cleverbutts.js
@@ -2,31 +2,46 @@ var Cleverbot = require('cleverbot-node');
 var Discordbot = require("discord.js");
 var fs = require("fs")
 var config = require("./config.json")
-var DBots = [new Discordbot.Client(),new Discordbot.Client(),new Discordbot.Client()]
-var CBots = [new Cleverbot,new Cleverbot,new Cleverbot]
+var availableAccounts = 0
+var DBots = [new Discordbot.Client(),new Discordbot.Client(),new Discordbot.Client(),new Discordbot.Client(),new Discordbot.Client()]
+var CBots = [new Cleverbot,new Cleverbot,new Cleverbot,new Cleverbot,new Cleverbot]
   , i = 0
   , callback = function callback(resp){
       setTimeout(function(str1, str2) {
         DBots[i].startTyping(config.botChannel);
         CBots[i].write(resp['message'],callback);
-        DBots[i = ( ( i + 1 ) %3)].sendMessage("194293716230471681", resp['message']);
+        DBots[i = ( ( i + 1 ) % availableAccounts)].sendMessage(config.botChannel, resp['message']);
       }, 2500);
       DBots[i].stopTyping(config.botChannel);
     };
+
 Cleverbot.prepare(function(){
   callback({message:config.startMessage})
 });
 
 DBots[0].on("ready", function(){
+  availableAccounts++
   console.log("[info] Bot 1 logged in as " + DBots[0].user.name + "#" + DBots[0].user.discriminator + " (" + DBots[0].user.id + ")")
 });
 
 DBots[1].on("ready", function(){
+  availableAccounts++
   console.log("[info] Bot 2 logged in as " + DBots[1].user.name + "#" + DBots[1].user.discriminator + " (" + DBots[1].user.id + ")")
 });
 
 DBots[2].on("ready", function(){
+  availableAccounts++
   console.log("[info] Bot 3 logged in as " + DBots[2].user.name + "#" + DBots[2].user.discriminator + " (" + DBots[2].user.id + ")")
+});
+
+DBots[3].on("ready", function(){
+  availableAccounts++
+  console.log("[info] Bot 4 logged in as " + DBots[3].user.name + "#" + DBots[3].user.discriminator + " (" + DBots[3].user.id + ")")
+});
+
+DBots[4].on("ready", function(){
+  availableAccounts++
+  console.log("[info] Bot 5 logged in as " + DBots[4].user.name + "#" + DBots[4].user.discriminator + " (" + DBots[4].user.id + ")")
 });
 
 DBots[0].on("message", function(message){
@@ -47,4 +62,6 @@ DBots[0].on("message", function(message){
 
 DBots[0].loginWithToken(config.bot1);
 DBots[1].loginWithToken(config.bot2);
-DBots[2].loginWithToken(config.bot3)
+DBots[2].loginWithToken(config.bot3);
+DBots[3].loginWithToken(config.bot4);
+DBots[4].loginWithToken(config.bot5);
