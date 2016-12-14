a468311d94fc2ffe7bbdd64f52bf5cb0a223e097
SplitPixl <splitpixl@gmail.com>
2016-12-13 17:55:35 -0800

    GUI Nearly complete
    Now to actually work on homework

--- main.js
+++ main.js
@@ -52,3 +52,5 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+global.bots = []
