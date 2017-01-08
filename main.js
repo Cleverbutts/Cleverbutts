091e4979af4054df4b1974516ef30957ad485e0b
SplitPixl <splitpixl@gmail.com>
2017-01-08 13:16:04 -0800

    Package.json was messed up...

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function createWindow () {
     slashes: true
   }))
 
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
